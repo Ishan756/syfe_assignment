65e6d1ea5158a130608ed1ef3a6410fee3782f8b
Ishan <ishanmishra756@gmail.com>
2026-05-24 04:38:41 +0530

    added deployed url in frontend for CORS

--- src/main/java/com/financemanager/config/SecurityConfig.java
+++ src/main/java/com/financemanager/config/SecurityConfig.java
@@ -67,7 +67,8 @@ public class SecurityConfig {
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
                 "http://localhost:4173",
-                "http://127.0.0.1:4173"
+            "http://127.0.0.1:4173",
+            "https://syfe-assignment-sand.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
